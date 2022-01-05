9953613e7a76a54f49e0a9d33424f9cfb76ab49d
Pedro Puado <pedro@cigaw.co.uk>
2022-01-05 12:45:29 +0000

    Add project files.

---
+++ MauiModalTest/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiModalTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnShowModalClicked(object sender, EventArgs e)
+	{
+		count++;
+
+        await Navigation.PushModalAsync(new ModalPage());
+    }
+}
+
---
+++ MauiModalTest/ModalPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MauiModalTest
+{
+    public class ModalPage : ContentPage
+    {
+        private readonly Label _errorText;
+
+        public ModalPage()
+        {
+            Background = Brush.White;
+            var button = new Button() { Text = "Close Modal" };
+            button.Clicked += Button_Clicked;
+            _errorText = new Label();
+            _errorText.TextColor = Colors.Black;
+
+            _errorText.Margin = new Thickness(20);
+
+            Content = new StackLayout
+            {
+                Children = {
+                    button, _errorText
+                }
+            };
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PopModalAsync(true);
+            }
+            catch (Exception exception)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Error: {exception.Message}");
+                sb.Append(exception.StackTrace);
+                _errorText.Text = sb.ToString();
+                Debug.WriteLine(exception);
+            }
+        }
+    }
+}
